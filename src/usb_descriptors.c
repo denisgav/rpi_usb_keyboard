6c948ee26a1eda804f250259166fe7054c6406b7
Denis Gavrysh <gavden@gmail.com>
2024-09-15 11:27:22 +0300

    Update usb_descriptors.c

--- src/usb_descriptors.c
+++ src/usb_descriptors.c
@@ -190,8 +190,8 @@ enum {
 char const *string_desc_arr[] =
 {
   (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
-  "TinyUSB",                     // 1: Manufacturer
-  "TinyUSB Device",              // 2: Product
+  "Raspberry Pi",                // 1: Manufacturer
+  "Pico Keyboard",               // 2: Product
   NULL,                          // 3: Serials will use unique ID if possible
 };
 
